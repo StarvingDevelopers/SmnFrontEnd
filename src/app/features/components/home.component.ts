991b99069f6571d07ef02a5e9ce23fa1a0b805ba
Murilo-Gotardo <murilogotardo08@gmail.com>
2024-06-23 19:34:18 -0300

    feat: Add more styles to home page

--- src/app/features/components/home.component.ts
+++ src/app/features/components/home.component.ts
@@ -2,13 +2,19 @@
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {NgOptimizedImage} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CardComponent} from "../../core/components/smn-card/card.component";
+import {OptionsComponent} from "../../core/components/smn-options/options.component";
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, RouterLink, FormsModule, ReactiveFormsModule, RouterLinkActive],
+  imports: [RouterOutlet, NgOptimizedImage, RouterLink, FormsModule, ReactiveFormsModule, RouterLinkActive, CardComponent, OptionsComponent],
   templateUrl: '../pages/home/home.component.html',
   styleUrl: '../../sass/main.css'
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+
+  colors = ["Red", "Blue", "White"];
+
+}
