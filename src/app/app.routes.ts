841591dda95065a213a1376c9fd3c767615f8b0d
Murilo-Gotardo <murilogotardo08@gmail.com>
2024-06-25 21:16:22 -0300

    feat(login): Make request to login and add implementation for auth service and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from "./features/components/login.component";
 import {RegisterComponent} from "./features/components/register.component";
 import {HomeComponent} from "./features/components/home.component";
+import {AuthGuardProviders} from "./core/guard/auth.guard";
 
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuardProviders] },
   { path: 'login',  component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent}
 ];
