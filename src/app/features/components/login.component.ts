2b10e36b92055049bb494e1ff6bdbcabbbc0f8fb
Murilo-Gotardo <murilogotardo08@gmail.com>
2024-06-25 21:16:22 -0300

    feat(login): Make request to login and add implementation for auth service and guard

--- src/app/features/components/login.component.ts
+++ src/app/features/components/login.component.ts
@@ -1,43 +1,50 @@
 ﻿import {Component, OnInit} from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {NgOptimizedImage} from "@angular/common";
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {DataService} from "../services/data.service";
+import {AuthService} from "../../core/services/auth.service";
 
 @Component({
   selector: 'login-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, RouterLink, FormsModule, ReactiveFormsModule, RouterLinkActive],
+  imports: [RouterOutlet, NgOptimizedImage, RouterLink, FormsModule, ReactiveFormsModule, RouterLinkActive, NgIf],
   templateUrl: '../pages/login/login.component.html',
   styleUrl: '../../sass/main.css'
 })
 
-export class LoginComponent{
+export class LoginComponent implements OnInit{
+  loginForm!: FormGroup;
+  authenticated = false;
 
-  authorizedLogin = false
+  constructor(private dataService: DataService, private fb: FormBuilder, private router: Router, private authService: AuthService) { }
 
-  login: FormGroup = new FormGroup({
-    userName: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required)
-  })
+  onSubmit() {
 
-  getLoginStatus(){
-    this.authorizedLogin = !!localStorage.getItem("login")
-  }
-
-  getLogindescription = () => this.authorizedLogin ? "authorized" : "unauthorized"
-
-  onSubmit(){
-    console.log(this.login.value)
-
-    //Make post
-    // posting...
-
-    if(this.authorizedLogin){
-      localStorage.clear()
-    } else {
-      localStorage.setItem("login", "yes")
+    const data = {
+      username:this.loginForm.get('username')?.value,
+      password:this.loginForm.get('password')?.value
     }
 
-    this.getLoginStatus()
+    this.dataService.postData(data).subscribe({
+        next: response => this.handleResponse(response),
+        error: err => console.log(err)
+      }
+    )
+  }
+
+  handleResponse(response: any) {
+
+
+    this.authService.login();
+    this.router.navigate(['/'])
+
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 }
