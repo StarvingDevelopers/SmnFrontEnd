b2d291a8a1f8caf7a0561f42e570ca8b13af68f3
murilo-gotardo <murilogotardo08@gmail.com>
2024-08-17 17:42:47 -0300

    feat: Add transition on mouse leave

--- src/app/core/components/smn-card/card.component.ts
+++ src/app/core/components/smn-card/card.component.ts
@@ -1,4 +1,4 @@
-﻿import {Component, Input, Renderer2, ElementRef} from '@angular/core';
+﻿import {Component, Input, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'smn-card',
@@ -13,14 +13,20 @@ export class CardComponent {
   constructor(private renderer: Renderer2) {}
 
   onMouseEnter() {
-    const gradient = document.querySelector('.gradient');
-    const groupsElement = document.querySelector('.groups');
+    const gradient = document.getElementById('gradient');
+    const groupsElement = document.getElementById('groups');
 
     if (gradient && groupsElement) {
       this.renderer.setStyle(groupsElement, 'z-index', '1');
       this.renderer.setStyle(gradient, 'background', `linear-gradient(to bottom, ${this.color} 1%, #343434 100%)`);
       this.renderer.setStyle(gradient, 'height', '100%');
     }
+  }
+
+  onMouseLeave() {
+    const gradient = document.getElementById('gradient');
+    this.renderer.setStyle(gradient, 'background', `linear-gradient(to bottom, ${this.color} 1%, #343434 100%)`);
+    this.renderer.setStyle(gradient, 'height', '0%');
   }
 }
 
