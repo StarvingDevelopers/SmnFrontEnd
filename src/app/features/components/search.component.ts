e7d11825ec79e3c604d5526aace9a2137006fc7d
murilo-gotardo <murilogotardo08@gmail.com>
2024-08-20 21:17:35 -0300

    feat: Pesquisa

--- src/app/features/components/search.component.ts
+++ src/app/features/components/search.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { SearchService } from '../services/search.service';
-import {NgForOf, NgIf} from "@angular/common"; // Supondo que você tenha um serviço para fazer a requisição
+import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
+import {ToastService} from "../../core/services/toast.service";
+import {CardComponent} from "../../core/components/smn-card/card.component";
 
 @Component({
   selector: 'search',
@@ -11,30 +13,45 @@ import {NgForOf, NgIf} from "@angular/common"; // Supondo que você tenha um ser
   imports: [
     ReactiveFormsModule,
     NgIf,
-    NgForOf
+    NgForOf,
+    CardComponent,
+    NgOptimizedImage
   ],
   styleUrls: ['../../sass/main.css']
 })
 export class SearchComponent implements OnInit {
   searchControl = new FormControl('');
   searchResults: any[] = [];
+  groups: Array<any> = [];
+  users: Array<any> = [];
 
-  constructor(private searchService: SearchService) {}
+  constructor(private searchService: SearchService, private toastService: ToastService) {}
 
   ngOnInit(): void {
     this.searchControl.valueChanges
       .pipe(
-        debounceTime(300), // Espera 300ms após o usuário parar de digitar
-        distinctUntilChanged() // Evita requisições desnecessárias se o valor não mudou
+        debounceTime(50),
+        distinctUntilChanged()
       )
       .subscribe(query => {
         if (query) {
           this.searchService.search(query).subscribe(results => {
-            this.searchResults = results;
+            this.searchResults.push(results)
+            this.groups = this.searchResults[0].groups
+            this.users = this.searchResults[0].users
           });
         } else {
           this.searchResults = [];
+          this.showError('A pesquisa nao retornou nada')
         }
       });
+  }
+
+  showError(message: string) {
+    this.toastService.showToast(message, 'error');
+  }
+
+  showSuccess(message: string) {
+    this.toastService.showToast(message, 'success');
   }
 }
--- src/app/features/services/search.service.ts
+++ src/app/features/services/search.service.ts
@@ -5,11 +5,12 @@ import {DataService} from "./data.service";
 @Injectable({
   providedIn: 'root'
 })
+
 export class SearchService {
 
   constructor(private dataService: DataService) { }
 
   search(query: string): Observable<any[]> {
-    return this.dataService.postData(query, '/search')
+    return this.dataService.getData('/search/' + query)
   }
 }
